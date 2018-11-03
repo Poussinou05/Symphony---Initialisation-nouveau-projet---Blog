b8157e211a456ce0fdff9822551acfc3ccab3b88
Lauriane Chauvet <pixelleart@gmail.com>
2018-11-03 12:53:17 +0100

    new HomeController with a new view

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder
+ * Date: 03/11/18
+ * Time: 12:31
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+
+    public function index()
+    {
+        $message = 'Hello WCS';
+        return $this->render('lucky/home.html.twig', ['message' => $message]);
+    }
+}
