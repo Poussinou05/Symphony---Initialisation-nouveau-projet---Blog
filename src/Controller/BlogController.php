1ba2d6218660a44b6ac1962fa81c7aa8cb18daf3
Lauriane Chauvet <pixelleart@gmail.com>
2018-11-12 12:41:52 +0100

    function show in CategoryController + view

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,21 +8,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog/{slug}", name="blog_show", requirements={"slug"="[a-z0-9-]+"})
      */
-    public function show($slug='article sans titre')
+//    public function show($slug='article sans titre')
+//    {
+//        $slug2 = str_replace("-"," ",$slug);
+//        $slug3 = ucwords($slug2);
+//
+//        return $this->render('blog/index.html.twig', ['slug' => $slug3]);
+//    }
+
+//    /**
+//     *@Route("/blog/categories", name="blog_listCategories"
+//     */
+//    public function listCategories()
+//    {
+//
+//        return $this->render('blog/listCategories.html.twig', ['categories' => $categories]);
+//    }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function show(Article $article) :Response
     {
-        $slug2 = str_replace("-"," ",$slug);
-        $slug3 = ucwords($slug2);
-
-        return $this->render('blog/index.html.twig', ['slug' => $slug3]);
+        return $this->render('blog/article.html.twig', ['article'=>$article]);
     }
-
-
 }
---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder
+ * Date: 12/11/18
+ * Time: 12:36
+ */
+
+namespace App\Controller;
+
+
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/category/{id}", name="category_show")
+     */
+    public function show(Category $category) :Response
+    {
+        return $this->render('blog/category.html.twig', ['category'=>$category]);
+    }
+
+}
